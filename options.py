cc438f825e2c4af8847b1345cb295dbf364bde7f
Qy_zzzz <68638337+Heprain@users.noreply.github.com>
2024-01-26 19:29:54 +0800

    Update options.py

--- options.py
+++ options.py
@@ -2,7 +2,6 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-# parser.add_argument('--file_root', default="./data", help='Data directory') 
 parser.add_argument('--inWidth', type=int, default=352, help='Width of RGB image') 
 parser.add_argument('--inHeight', type=int, default=352, help='Height of RGB image') 
 parser.add_argument('--max_epochs', type=int, default=60, help='Max. number of epochs') 
@@ -14,26 +13,25 @@ parser.add_argument('--lr_mode', default='step', help='Learning rate policy, ste
 parser.add_argument('--savedir', default='./results', help='Directory to save the results') 
 parser.add_argument('--resume', default=None, help='Use this checkpoint to continue training')
 parser.add_argument('--logFile', default='trainValLog.txt', help='File that stores the training and validation logs')
-parser.add_argument('--onGPU', default=True, type=lambda x: (str(x).lower() == 'true'),
-                    help='Run on CPU or GPU. If TRUE, then GPU.')
+parser.add_argument('--gpu', default=True, type=lambda x: (str(x).lower() == 'true'),
+                    help='Run on CPU or GPU. If TRUE, then GPU')
 parser.add_argument('--weight', default='', type=str, help='pretrained weight, can be a non-strict copy')
 parser.add_argument('--depth', type=int, default=1, help='use RGB-D data, default True')
 parser.add_argument('--save_depth', default=0, type=int)
-parser.add_argument('--depth_weight', type=float, default=0.3, help='idr loss weight, default 0.3')
-# parser.add_argument('--ms', type=int, default=0, help='apply multi-scale training, default False')  
-    
-parser.add_argument('--rgb_root', type=str, default='../Dataset/RGB-D/Train/RGB/', help='the training rgb images root')
-parser.add_argument('--depth_root', type=str, default='../Dataset/RGB-D/Train/depth/', help='the training depth images root')
-parser.add_argument('--gt_root', type=str, default='../Dataset/RGB-D/Train/GT/', help='the training gt images root')
-parser.add_argument('--test_rgb_root', type=str, default='../Dataset/RGB-D/test_in_train/RGB/', help='the test rgb images root')
-parser.add_argument('--test_depth_root', type=str, default='../Dataset/RGB-D/test_in_train/depth/', help='the test depth images root')
-parser.add_argument('--test_gt_root', type=str, default='../Dataset/RGB-D/test_in_train/GT/', help='the test gt images root')
-parser.add_argument('--criterion', type=str, default='API', help='API or bce')
-parser.add_argument('--gpu', default=True, type=lambda x: (str(x).lower() == 'true'),
-                    help='Run on CPU or GPU. If TRUE, then GPU')
-
-parser.add_argument('--test_path',type=str,default='../Dataset/RGB-D/Test/',help='test dataset path')
+parser.add_argument('--depth_weight', type=float, default=0.3, help='idr loss weight, default 0.3') 
+parser.add_argument('--criterion', type=str, default='API', help='API or bce') 
 parser.add_argument('--pretrained', default=None, help='Pretrained model')
+
+parser.add_argument('--rgb_root', type=str, default='../Dataset/RGB-T/Train/RGB/', help='the training rgb images root')
+parser.add_argument('--depth_root', type=str, default='../Dataset/RGB-T/Train/T/', help='the training depth images root')
+parser.add_argument('--gt_root', type=str, default='../Dataset/RGB-T/Train/GT/', help='the training gt images root')
+parser.add_argument('--val_rgb_root', type=str, default='../Dataset/RGB-D/test_in_train/RGB/', help='the test rgb images root')
+parser.add_argument('--val_depth_root', type=str, default='../Dataset/RGB-D/test_in_train/depth/', help='the test depth images root')
+parser.add_argument('--val_gt_root', type=str, default='../Dataset/RGB-D/test_in_train/GT/', help='the test gt images root')
+parser.add_argument('--test_path',type=str,default='../Dataset/RGB-T/Test/',help='test dataset path')
+
+
+parser.add_argument('--rgbt', default=0)
 
 parser.add_argument('--net_size', type=str, default='m', help='The size of checkpoints(m/s/t)')
 
